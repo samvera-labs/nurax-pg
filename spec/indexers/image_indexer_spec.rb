9c792101ff808f6f681dcd8d4dbc9814ae90a4ae
E. Lynette Rayle <elr37@cornell.edu>
2021-09-11 10:48:38 -0400

    generate image work resource
    NOTE: Tests fail…
    
    * requires `let(:resource) { image }`
    * unable to find `test_adapter`

---
+++ spec/indexers/image_indexer_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Generated via
+#  `rails generate hyrax:work_resource Image`
+require 'rails_helper'
+require 'hyrax/specs/shared_specs/indexers'
+
+RSpec.describe ImageIndexer do
+  let(:indexer_class) { described_class }
+
+  it_behaves_like 'a Hyrax::Resource indexer'
+end
