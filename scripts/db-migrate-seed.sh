8795f5dcc0b03be6263c0610a9fe169109b54b7d
Alexandra Dunn <dunn.alex@gmail.com>
2021-11-30 13:27:20 -0800

    seeds: create initial administrativeset, assign admin to dev user

--- scripts/db-migrate-seed.sh
+++ scripts/db-migrate-seed.sh
@@ -4,8 +4,6 @@ set -e
 db-wait.sh "$DB_HOST:$DB_PORT"
 bundle exec rails db:create
 bundle exec rails db:migrate
-# bundle exec sequel -m db/sequel postgres://"$DB_USERNAME":"$DB_PASSWORD"@"$DB_HOST"/"$METADATA_DATABASE_NAME"
 
-# db-wait.sh "$SOLR_HOST:$SOLR_PORT"
-
-# bundle exec rails db:seed
+db-wait.sh "$SOLR_HOST:$SOLR_PORT"
+bundle exec rails db:seed
