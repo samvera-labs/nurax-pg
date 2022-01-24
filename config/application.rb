a375d38e1ad7ebf8fb748f5bbec19a092d59f36e
Alexandra Dunn <dunn.alex@gmail.com>
2022-01-24 09:36:37 -0800

    docker: log to stdout

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,12 @@ module NuraxPg
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
+    if ENV["RAILS_LOG_TO_STDOUT"]
+      logger = ActiveSupport::Logger.new($stdout)
+      logger.formatter = config.log_formatter
+      config.logger = ActiveSupport::TaggedLogging.new(logger)
+    end
+
     # The locale is set by a query parameter, so if it's not found render 404
     config.action_dispatch.rescue_responses.merge!(
       'I18n::InvalidLocale' => :not_found
